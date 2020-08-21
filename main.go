855ebb29235d7809881ea81055cc1cba8d63e5cd
jefferson otoni <jeff.otoni@s3wf.com.br>
2020-08-21 18:48:17 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,199 @@
+package concat
+
+import (
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+//buildStr monta a string
+func buildStr(str interface{}) string {
+
+	switch str.(type) {
+	case nil:
+		return "nil"
+	//
+	case bool:
+		return strconv.FormatBool(bool(str.(bool)))
+	//
+	case int:
+		return strconv.Itoa(int(str.(int)))
+
+	case []int:
+		return IntToStringFast(str.([]int))
+	case uint:
+		return strconv.FormatUint(uint64(str.(uint)), 10)
+	case []uint:
+		concat := ""
+		for _, val := range str.([]uint) {
+			concat = Concat(concat, val)
+		}
+		return concat
+	case int8:
+		return strconv.Itoa(int(str.(int8)))
+	//provavelmente funciona para byte, pois é um aliais para uint8
+	case uint8:
+		return strconv.FormatUint(uint64(str.(uint8)), 10)
+	case []int8:
+		concat := ""
+		for _, val := range str.([]int8) {
+			concat = Concat(concat, val)
+		}
+		return concat
+
+	case []uint8:
+		concat := ""
+		for _, val := range str.([]uint8) {
+			concat = Concat(concat, val)
+		}
+		return concat
+
+	case int16:
+		return strconv.Itoa(int(str.(int16)))
+	case uint16:
+		return strconv.FormatUint(uint64(str.(uint16)), 10)
+	case []int16:
+		concat := ""
+		for _, val := range str.([]int16) {
+			concat = Concat(concat, val)
+		}
+		return concat
+
+	case []uint16:
+		concat := ""
+		for _, val := range str.([]uint16) {
+			concat = Concat(concat, val)
+		}
+		return concat
+
+	//probably work for rune too, since rune is a alias for int32
+	case int32:
+		return strconv.FormatInt(int64(str.(int32)), 10)
+	case uint32:
+		return strconv.FormatUint(uint64(str.(uint32)), 10)
+	case []int32:
+		return Int32ToStringFast(str.([]int32))
+	case []uint32:
+		concat := ""
+		for _, val := range str.([]uint32) {
+			concat = Concat(concat, val)
+		}
+		return concat
+	case int64:
+		return strconv.FormatInt(int64(str.(int64)), 10)
+	case uint64:
+		return strconv.FormatUint(uint64(str.(uint64)), 10)
+	case []int64:
+		concat := ""
+		for _, val := range str.([]int64) {
+			concat = Concat(concat, val)
+		}
+		return concat
+	case []uint64:
+		concat := ""
+		for _, val := range str.([]uint64) {
+			concat = Concat(concat, val)
+		}
+		return concat
+
+	case string:
+		return string(str.(string))
+
+	case []string:
+		return strings.Join(str.([]string), " ")
+
+	case float64:
+		return strconv.FormatFloat(str.(float64), 'f', 6, 64)
+	case []float64:
+		concat := ""
+		for _, val := range str.([]float64) {
+			concat = Concat(concat, val)
+		}
+		return concat
+	case float32:
+		return strconv.FormatFloat(float64(str.(float32)), 'f', 6, 64)
+	case []float32:
+		concat := ""
+		for _, val := range str.([]float32) {
+			concat = Concat(concat, val)
+		}
+		return concat
+	case uintptr:
+		return string(str.(uintptr))
+	case []uintptr:
+		concat := ""
+		for _, val := range str.([]uintptr) {
+			concat = Concat(concat, val)
+		}
+		return concat
+	case complex64:
+		return "not suport complex 64"
+	case complex128:
+		return "not suport complex 128"
+	default:
+		println("type no encontrado...")
+		break
+	}
+	return ""
+}
+
+func IntToStringFast(a []int) string {
+	if len(a) == 0 {
+		return ""
+	}
+	b := make([]string, len(a))
+	for i, v := range a {
+		b[i] = strconv.Itoa(v)
+	}
+	return strings.Join(b, ",")
+}
+
+func Int32ToStringFast(a []int32) string {
+	if len(a) == 0 {
+		return ""
+	}
+
+	b := make([]string, len(a))
+	for i, v := range a {
+		b[i] = strconv.FormatInt(int64(v), 10)
+	}
+	return strings.Join(b, ",")
+}
+
+//Stdout func
+func Stdout(strs ...interface{}) {
+	var sb strings.Builder
+	for _, str := range strs {
+		sb.WriteString(buildStr(str))
+	}
+	io.Copy(os.Stdout, strings.NewReader(sb.String()))
+}
+
+//Concat contaquena
+func Concat(strs ...interface{}) string {
+	var sb strings.Builder
+	for _, str := range strs {
+		sb.WriteString(buildStr(str))
+	}
+	return sb.String()
+}
+
+//Println printa com n\
+func Println(strs ...interface{}) {
+	var sb strings.Builder
+	for _, str := range strs {
+		sb.WriteString(buildStr(str))
+	}
+	sb.WriteString("\n")
+	io.Copy(os.Stdout, strings.NewReader(sb.String()))
+}
+
+//Print printa
+func Print(strs ...interface{}) {
+	var sb strings.Builder
+	for _, str := range strs {
+		sb.WriteString(buildStr(str))
+	}
+	io.Copy(os.Stdout, strings.NewReader(sb.String()))
+}
---
+++ main_test.go
@@ -0,0 +1,175 @@
+package concat
+
+import (
+	"testing"
+)
+
+func Test_buildStr(t *testing.T) {
+	var many1String, many1Int, many1Int8, many1Int16, many1Int32, many1Int64 interface{}
+	var many1Uint, many1Uint8, many1Uint16, many1Uint32, many1Uint64, many1Uintptr interface{}
+	var many1Float32, many1Float64 interface{}
+
+	many1String = string("21")
+	many1Int = int(100)
+	many1Int8 = int8(100)
+	many1Int16 = int16(100)
+	many1Int32 = int32(100)
+	many1Int64 = int64(100)
+	many1Uint = uint(100)
+	many1Uint8 = uint8(100)
+	many1Uint16 = uint16(100)
+	many1Uint32 = uint32(100)
+	many1Uint64 = uint64(100)
+	many1Uintptr = uintptr(100)
+
+	many1Float32 = float32(100.01)
+	many1Float64 = float64(100.01)
+	type args struct {
+		str interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{"test_buildStr_1", args{str: many1String}, "21"},
+		{"test_buildStr_2", args{str: many1Int}, "100"},
+		{"test_buildStr_3", args{str: many1Int8}, "100"},
+		{"test_buildStr_4", args{str: many1Int16}, "100"},
+		{"test_buildStr_5", args{str: many1Int32}, "100"},
+		{"test_buildStr_6", args{str: many1Int64}, "100"},
+		{"test_buildStr_7", args{str: many1Uint}, "100"},
+		{"test_buildStr_8", args{str: many1Uint8}, "100"},
+		{"test_buildStr_9", args{str: many1Uint16}, "100"},
+		{"test_buildStr_10", args{str: many1Uint32}, "100"},
+		{"test_buildStr_11", args{str: many1Uint64}, "100"},
+		{"test_buildStr_12", args{str: many1Uintptr}, "d"}, //endereço do ponteiro
+		{"test_buildStr_13", args{str: many1Float32}, "100.010002"},
+		{"test_buildStr_14", args{str: many1Float64}, "100.010000"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := buildStr(tt.args.str); got != tt.want {
+				t.Errorf("buildStr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStdout(t *testing.T) {
+	var manyStdout []interface{}
+	manyStdout = append(manyStdout, []string{"1"})
+
+	type args struct {
+		strs []interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+		{"test_stdout_1", args{strs: manyStdout}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Stdout(tt.args.strs...)
+		})
+	}
+}
+
+func TestConcat(t *testing.T) {
+
+	var many1String, many1Int, many1Int8, many1Int16, many1Int32, many1Int64 []interface{}
+	var many1Uint, many1Uint8, many1Uint16, many1Uint32, many1Uint64, many1Uintptr []interface{}
+	var many1Float32, many1Float64 []interface{}
+
+	many1String = append(many1String, []string{"21", "joao", "beta"})
+	many1Int = append(many1Int, []int{100, 98, 23})
+	many1Int8 = append(many1Int8, []int8{100, 98, 23})
+	many1Int16 = append(many1Int16, []int16{100, 98, 23})
+	many1Int32 = append(many1Int32, []int32{100, 98, 23})
+	many1Int64 = append(many1Int64, []int64{100, 98, 23})
+	many1Uint = append(many1Uint, []uint{100, 98, 23})
+	many1Uint8 = append(many1Uint8, []uint8{100, 98, 23})
+	many1Uint16 = append(many1Uint16, []uint16{100, 98, 23})
+	many1Uint32 = append(many1Uint32, []uint32{100, 98, 23})
+	many1Uint64 = append(many1Uint64, []uint64{100, 98, 23})
+	many1Uintptr = append(many1Uintptr, []uintptr{100})
+
+	many1Float32 = append(many1Float32, []float32{100.01, 100.01})
+	many1Float64 = append(many1Float64, []float64{100.01, 100.01})
+	type args struct {
+		strs []interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{"test_concat_1", args{strs: many1String}, "21joaobeta"},
+		{"test_concat_2", args{strs: many1Int}, "1009823"},
+		{"test_concat_3", args{strs: many1Int8}, "1009823"},
+		{"test_concat_4", args{strs: many1Int16}, "1009823"},
+		{"test_concat_5", args{strs: many1Int32}, "1009823"},
+		{"test_concat_6", args{strs: many1Int64}, "1009823"},
+		{"test_concat_7", args{strs: many1Uint}, "1009823"},
+		{"test_concat_8", args{strs: many1Uint8}, "1009823"},
+		{"test_concat_9", args{strs: many1Uint16}, "1009823"},
+		{"test_concat_10", args{strs: many1Uint32}, "1009823"},
+		{"test_concat_11", args{strs: many1Uint64}, "1009823"},
+		{"test_concat_12", args{strs: many1Uintptr}, "d"}, //endereço do ponteiro
+		{"test_concat_13", args{strs: many1Float32}, "100.010002100.010002"},
+		{"test_concat_14", args{strs: many1Float64}, "100.010000100.010000"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Concat(tt.args.strs...); got != tt.want {
+				t.Errorf("Concat() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPrintln(t *testing.T) {
+	var manyPrintln []interface{}
+	manyPrintln = append(manyPrintln, []string{"1", "2"})
+
+	type args struct {
+		strs []interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+		{"test_println_1", args{strs: manyPrintln}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Println(tt.args.strs...)
+		})
+	}
+}
+
+func TestPrint(t *testing.T) {
+	var manyPrint []interface{}
+	manyPrint = append(manyPrint, []string{"1", "2"})
+	type args struct {
+		strs []interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+		{"test_print_1", args{strs: manyPrint}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Print(tt.args.strs...)
+		})
+	}
+}
